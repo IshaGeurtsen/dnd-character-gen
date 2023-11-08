d5b9c506ee27b7c7c5ede997a1ca3010638985c7
Isha Geurtsen <139a414@gmail.com>
2023-11-08 19:05:59 +0100

    make srd more easily parsable

---
+++ scripts/srd_pdf2txt.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+from os import fspath
+from tika import parser
+import re
+import string
+
+translation_table = str.maketrans(
+    {
+        0xA0: " ",
+        0xAD: None,
+        0xD7: "*",
+        0x336: "-",
+        0x2010: "-",
+        0x2011: "-",
+        0x2012: "-",
+        0x2013: "-",
+        0x2014: "-",
+        0x2019: "'",
+        0x201C: '"',
+        0x201D: '"',
+        0x2022: "*",
+        0x2028: "\n",
+        0x2212: "-",
+    }
+)
+
+
+def improve_text(text):
+    text = text.translate(translation_table)
+    text = text.replace(chr(0x2026), "...")
+    # remove a lot of empty newlines
+    text = re.sub("\n{2,}", "\n\n", text)
+    # join paragraphs
+    text = re.sub("\t\r  ", " ", text)
+    text = re.sub(r"(?<=\w) \n(?=[a-z])", " ", text)
+    # replace remaining tabs with spaces
+    text = re.sub(r"\n\t(?=\w)", "\n    ", text)
+    # shorten multiple dashes
+    text = re.sub("--{2,}", "-", text)
+    # remove trailing whitespace
+    text = re.sub(" +(?=\n)", "", text)
+    text = text.strip() + "\n"
+    return text
+
+
+if __name__ == "__main__":
+    cwd = Path.cwd()
+
+    srd = cwd / "doc" / "SRD_CC_v5.1.pdf"
+    srd_text = cwd / "doc" / "gen" / "SRD.txt"
+
+    parsed = parser.from_file(fspath(srd))
+    text = parsed["content"]
+
+    text = improve_text(text)
+
+    assert isinstance(text, str)
+    known = {
+        *string.digits,
+        *string.ascii_letters,
+        *string.punctuation,
+        "\n",
+        " ",
+        "\r",
+        chr(189),
+    }
+    unkown = set(text) - known
+    assert not unkown
+
+    srd_text.unlink()
+    with open(srd_text, "xb") as out:
+        out.write(text.encode(encoding="utf-8"))
