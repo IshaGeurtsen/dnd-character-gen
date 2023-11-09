c1112ec685d9aeaff4869edb009bdacc0631e2d6
Isha Geurtsen <139a414@gmail.com>
2023-11-09 13:47:56 +0100

    make the server respond with static content

--- src/dnd_character_gen_steviestickman/__main__.py
+++ src/dnd_character_gen_steviestickman/__main__.py
@@ -1,9 +1,14 @@
 from typing import NoReturn
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import (
+    HTTPServer,
+    BaseHTTPRequestHandler,
+    SimpleHTTPRequestHandler,
+)
 from http import HTTPStatus
 import sys
-from typing import NewType, Any, BinaryIO
-
+from typing import NewType, Any, BinaryIO, cast
+from pathlib import Path
+from os import fspath
 
 Ip = NewType("Ip", str)
 Port = NewType("Port", int)
@@ -18,17 +23,58 @@ class Handler(BaseHTTPRequestHandler):
     rfile: BinaryIO
     wfile: BinaryIO
     protocol_version: str
+    src = Path(__file__).parent
+    static = (src / "static").resolve()
+    extensions_map = SimpleHTTPRequestHandler.extensions_map
+
+    def send_document(
+        self,
+        code: HTTPStatus,
+        mime: str,
+        doc: BinaryIO,
+    ) -> None:
+        binary = doc.read()
+        self.send_response(code)
+        self.send_header("Content-Type", mime)
+        self.send_header("Content-Length", str(len(binary)))
+        self.end_headers()
+        self.flush_headers()
+        self.wfile.write(binary)
+
+    def guess_type(self, path: str) -> str:
+        return SimpleHTTPRequestHandler.guess_type(
+            cast(SimpleHTTPRequestHandler, self), path
+        )
 
     def do_GET(self) -> None:
-        if "text/html" in self.headers["Accept"]:
-            self.send_response(HTTPStatus.OK)
-            self.send_header("Content-Type", "text/html; charset=utf8")
-            self.send_header("Content-Length", str(len("<p>hello world</p>")))
-            self.end_headers()
-            self.flush_headers()
-            self.wfile.write(b"<p>hello world</p>")
-        else:
-            self.send_error(HTTPStatus.NOT_ACCEPTABLE)
+        parts = self.path.lstrip("/").split("/")
+        print(parts, self.path)
+        match parts:
+            case ["dynamic"]:
+                raise NotImplementedError
+            case ["static", ""]:
+                with (self.static / "index.html").open("rb") as doc:
+                    self.send_document(HTTPStatus.OK, "text/html", doc)
+            case ["favicon.ico"]:
+                with (self.static / "favicon.png").open("rb") as doc:
+                    self.send_document(HTTPStatus.OK, "image/png", doc)
+            case ["static", *parts]:
+                try:
+                    path = self.static / "/".join(parts)
+                    with path.open("rb") as doc:
+                        self.send_document(
+                            HTTPStatus.OK,
+                            self.guess_type(fspath(path)),
+                            doc,
+                        )
+                except FileNotFoundError:
+                    self.log_error(f"could not find {self.path}")
+                    self.send_error(HTTPStatus.NOT_FOUND)
+            case [""]:
+                self.send_response(HTTPStatus.FOUND)
+                self.send_header("Location", "/static/")
+                self.end_headers()
+                self.flush_headers()
 
 
 def run(
@@ -39,7 +85,7 @@ def run(
 ) -> NoReturn:
     server_address = ip, port
     httpd = server_class(server_address, handler_class)
-    print(f"listening on https://{ip}:{port}")
+    print(f"listening on http://{ip}:{port}")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
